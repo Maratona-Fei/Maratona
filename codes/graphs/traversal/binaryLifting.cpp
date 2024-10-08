756c989c9414d815b194d65f1989092f8732837e
albertx0 <126724209+albertx0@users.noreply.github.com>
2024-10-08 15:47:18 -0300

    Update binaryLifting.cpp

--- codes/graphs/traversal/binaryLifting.cpp
+++ codes/graphs/traversal/binaryLifting.cpp
@@ -1,23 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define endl '\n'
-
-#define f first
-#define s second
-
-#define vi vector<int>
-#define grafo vector<vector<int>>
-
-#define dbg(x) cout << #x << " = " << x << endl;
-
-typedef long long ll;
-
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-
 class Tree {
 	private:
 		const int root = 0;
@@ -101,7 +81,3 @@ class Tree {
 			return dep[n1]+dep[n2] - dep[lca(n1,n2)]*2;
 		}
 };
-			
-int main(){ _
-	return 0;
-}
