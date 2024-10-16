ae1179198bce8fa0201390a9f7587f877f3a96ed
albertx0 <126724209+albertx0@users.noreply.github.com>
2024-10-16 17:11:02 -0300

    Update geometria2d.cpp

--- codes/templates/geometria2d.cpp
+++ codes/templates/geometria2d.cpp
@@ -1,7 +1,5 @@
 typedef double T;
 
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 const double eps = 1e-9;
 const double pi = acos((double)-1.0);
 
@@ -10,7 +8,7 @@ int sign(double x) {return (x > eps) - (x < -eps);}
 
 struct pt{
 	T x, y;
-	
+
 	pt(T x, T y) : x(x), y(y) {}
 	pt() {x=0,y=0;}
 	pt operator+(pt p) {return {x+p.x, y+p.y};}
@@ -121,7 +119,7 @@ pt perp(pt a) {return pt(-a.y, a.x);}
 
 void polarSort(vector<pt> &v){ // Sort antiHorario
 	sort(v.begin(), v.end(), [](pt a, pt b){
-		return make_tuple(half(a), 0.0, a.norm2()) < 
+		return make_tuple(half(a), 0.0, a.norm2()) <
 			   make_tuple(half(b), cross(a, b), b.norm2());
 	});
 }
@@ -147,4 +145,216 @@ bool isConvex(vector<pt> p){
 	}
 
 	return !(hasPos and hasNeg);
+}
+
+struct line {
+	pt a, b;//vai do ponto A ao B
+	pt v; T c;//direcao do vetor [cross] (x, y) = c	
+
+	line() {}
+
+	line(pt v, T c) : v(v), c(c) {
+		auto p = get_points();
+		a = p.f; b = p.s;
+	}
+	//eq ax + by + c = 0
+	line(T _a, T _b, T, T _c) : v({_b, -_a}), c(-_c) {
+		auto p = get_points();
+		a = p.f; b = p.s;
+	}
+	//passa pelos pontos p e q
+	line(pt p, pt q) : v(q-p), c(cross(v, p)), a(p), b(q) {}
+	pair<pt , pt> get_points() { //extrai qualquer dois pontos dessa linha
+	pt p, q; T a = -v.y, b = v.x; //ax+by = c
+	if(sign(a) == 0) {
+		p = pt(0, c/b);
+		q = pt(1, c/b);
+	}
+	else if(sign(b) == 0) {
+		p = pt(c/a, 0);
+		q = pt(c/a, 1);
+	}
+	else {
+		p = pt(0, c/b);
+		q = pt(1, (c-a)/b);
+	}
+
+	return {p, q};
+	}
+
+	//ax + by + c = 0
+	array<T, 3> get_abc() {
+		T a = -v.y, b = v.x;
+		return {a, b, -c};
+	}
+
+	//1 se esta na esquerda, -1 se esta na direita, 0 se estiver na linha
+	int side(pt p) {return sign(cross(v, p) -c);}
+	//reta perpendicular a esta e passa pelo ponto p
+	line perpendicular_through(pt p) {return {p, p+perp(v)};}
+	//faz a translacao da linha pelo vetor t i.e fznd o shift com o vetor t
+	line translate(pt t) {return {v, c+cross(v, t)};}
+	//compara dois pontos com suas proj. ortogonais nessa linha
+	//um ponto de projecao vem antes de outro se vier primeiro de acordo com o vetor
+	bool cmp_by_projection(pt p, pt q) {return dot(v, p) < dot(v, q);}
+	line shift_left(T d) {
+		pt z = v.perp().truncate(d);
+		return line(a+z, b+z);
+	}
+};
+
+//encontra um ponto de A ate B com distancia d
+pt point_along_line(pt a, pt b, T d) {
+	assert(a != b);
+	return a + (((b - a) / (b - a).norm()) * d);
+}
+//projecao do ponto c na linha que passa por A e B 
+//assumindo que A!=B
+pt project_from_point_to_line(pt a, pt b, pt c) {
+	return a + (b - a) * dot(c - a, b - a) / (b - a).norm2();
+}
+//ponto de reflexao C que passa na linha que passa por A e B
+//assumindo que A!=B
+pt reflection_from_point_to_line(pt a, pt b, pt c) {
+	pt p = project_from_point_to_line(a, b, c);
+	return p + p - c;
+}
+//dist min do ponto  ate a linha que passa por A e B
+T dist_from_point_to_line(pt a, pt b, pt c) {
+	return fabs(cross(b - a, c - a) / (b - a).norm());
+}
+//retorna true se o ponto P pertence ao segmento AB
+bool is_point_on_seg(pt a, pt b, pt p) {
+	if(fabs(cross(p - b, a - b)) < eps) {
+		if(p.x < min(a.x, b.x) - eps or p.x > max(a.x, b.x) + eps) return false;
+		if(p.y < min(a.y, b.y) - eps or p.y > max(a.y, b.y) + eps) return false;
+		return true;
+	}
+	return false;
+
+}
+//pont com dist min de um ponto c ate o seg AB que pertence ao seg AB
+pt project_from_point_to_seg(pt a, pt b, pt c) {
+	T r = dist2(a, b);
+	if(sign(r) == 0) return a;
+	r = dot(c - a, b - a) / r;
+	if(r < 0) return a;
+	if(r > 1) return b;
+	return a + (b - a) * r;
+}
+//dist min de um ponto c ate um seg AB
+T dist_from_point_to_seg(pt a, pt b, pt c) {
+	return dist(c, project_from_point_to_seg(a, b, c));
+}
+
+// 0 se nao eh paralelo, 1 se eh paralelo, 2 se eh colinear
+int is_parallel(pt a, pt b, pt c, pt d) {
+	T k = fabs(cross(b - a, d - c));
+	if(k < eps) {
+		if(fabs(cross(a - b, a - c)) < eps and fabs(cross(c - d, c - a)) < eps) return 2;
+		else return 1;
+	}
+	else return 0;
+}
+//checa se duas linhas sao as mesmas
+bool are_lines_same(pt a, pt b, pt c, pt d) {
+	if(fabs(cross(a - c, c - d)) < eps and fabs(cross(b - c, c - d)) < eps) return true;
+	return false;
+}
+//vetor bissetriz de <abc
+pt angle_bisector(pt& a, pt& b, pt& c) {
+	pt p = a - b, q = c - b;
+	return p + q * sqrt(dot(p, p) / dot(q, q));
+}
+//1 se o ponto eh ccw com a linha, 2 se o ponto eh cw da linha
+//3 se o ponto esta na linha
+int point_line_relation(pt a, pt b, pt p) {
+	int c = sign(cross(p - a, b - a));
+	if(c < 0) return 1;
+	if(c > 0) return 2;
+	return 3;
+}
+//ponto de intersec entre uma linha AB e uma linha CD 
+//assumindo que existe apenas uma intersec
+bool line_line_intersection(pt a, pt b, pt c, pt d, pt& ans) {
+	T a1 = a.y - b.y, b1 = b.x - a.x, c1 = cross(a, b);
+	T a2 = c.y - d.y, b2 = d.x - c.x, c2 = cross(c, d);
+	T det = a1 * b2 - a2 * b1;
+	if(det == 0) return 0;
+	ans = pt((b1 * c2 - b2 * c1) / det, (c1 * a2 - a1 * c2) / det);
+	return 1;
+}
+//ponto de intersec entre um seg AB e um seg CD 
+//assumindo que existe apenas uma intersec
+bool seg_seg_intersection(pt a, pt b, pt c, pt d, pt& ans) {
+	T oa = cross2(c, d, a), ob = cross2(c, d, b);
+	T oc = cross2(a, b, c), od = cross2(a, b, d);
+	if(oa * ob < 0 and oc * od < 0) {
+		ans = (a * ob - b * oa) / (ob - oa);
+		return 1;
+	}
+	else return 0;
+}
+//ponto de intersec entre um seg AB e um seg CD 
+//assumindo que poder ter mais de uma intersec
+//se.size() == 0, sem intersecs
+//se.size() == 1, uma intersec
+//se.size() == 2, possui um intervalo de intersecs
+set<pt> seg_seg_intersection_inside(pt a, pt b, pt c, pt d) {
+	pt ans;
+	if(seg_seg_intersection(a, b, c, d, ans)) return {ans};
+	set<pt> se;
+	if(is_point_on_seg(c, d, a)) se.insert(a);
+	if(is_point_on_seg(c, d, b)) se.insert(b);
+	if(is_point_on_seg(a, b, c)) se.insert(c);
+	if(is_point_on_seg(a, b, d)) se.insert(d);
+
+	return se;
+}
+//intersec entre seg AB e linha CD
+//0 se nao se intersec, 1 se houver intersec adequada, 2 se o seg intersecta
+int seg_line_relation(pt a, pt b, pt c, pt d) {
+	T p = cross2(c, d, a);
+	T q = cross2(c, d, b);
+	if(sign(p) == 0 and sign(q) == 0) return 2;
+	else if(p * q < 0) return 1;
+	else return 0;
+}
+//intersec entre seg AB e linha CD 
+//assumindo uma unica intersec
+bool seg_line_intersection(pt a, pt b, pt c, pt d, pt& ans) {
+	bool k = seg_line_relation(a, b, c, d);
+	assert(k != 2);
+	if(k) line_line_intersection(a, b, c, d, ans);
+	return k;
+}
+//dist min de um seg AB e um seg CD
+T dist_from_seg_to_seg(pt a, pt b, pt c, pt d) {
+	pt dummy;
+	if(seg_seg_intersection(a, b, c, d, dummy)) return 0.0;
+	return min({dist_from_point_to_seg(a, b, c), dist_from_point_to_seg(a, b, d), dist_from_point_to_seg(c, d, a), dist_from_point_to_seg(c, d, b)});
+}
+//dist min de um ponto c ate o raio
+//(comecando do ponto a e direcao do vetor b)
+T dist_from_point_to_ray(pt a, pt b, pt c) {
+	b = a + b;
+	T r = dot(c - a, b - a);
+	if(r < 0.0) return dist(c, a);
+	return dist_from_point_to_line(a, b, c);
+}
+//comecando ponto as e direcao do vetor ad
+bool ray_ray_intersection(pt as, pt ad, pt bs, pt bd) {
+	T dx = bs.x - as.x, dy = bs.y - as.y;
+	T det = bd.x * ad.y - bd.y * ad.x;
+	if(fabs(det) < eps) return 0;
+	T u = (dy * bd.x - dx * bd.y) / det;
+	T v = (dy * ad.x - dx * ad.y) / det;
+	if(sign(u) >= 0 and sign(v) >= 0) return 1;
+	else return 0;
+}
+double ray_ray_distance(pt as, pt ad, pt bs, pt bd) {
+	if(ray_ray_intersection(as, ad, bs, bd)) return 0.0;
+	T ans = dist_from_point_to_ray(as, ad, bs);
+	ans = min(ans, dist_from_point_to_ray(bs, bd, as));
+	return ans;
 }
