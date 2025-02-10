9f5233fe1aad2fd1a2f0e669e05ef0b504de0ff0
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-02-10 16:27:01 -0300

    Update dijkstra.cpp

--- codes/graphs/dijkstra.cpp
+++ codes/graphs/dijkstra.cpp
@@ -1,25 +1,24 @@
 // Complexidade: O(m log(n))
-
 #define pp pair<ll,int>
 
 vector<vector<pp>>g;
 
 vector<ll> dijkstra(int s){
 	// {dist, v}
-	priority_queue<pp> pq;
+	priority_queue<pp, vector<pp>, greater<pp>> pq;
 
 	vector<ll> d(g.size(), LINF);
 	d[s] = 0, pq.push({0, s});
 
 	while(!pq.empty()){
 		auto [dist, u] = pq.top(); pq.pop();
-		if(-dist > d[u])continue;
+		if(dist > d[u])continue;
 
 		for(auto [v, w] : g[u]){
 			int sum = d[u] + w;
 			if(d[v] > sum){
 				d[v] = sum;
-				pq.push({-sum, v});
+				pq.push({sum, v});
 			}
 		} 
 	}	
