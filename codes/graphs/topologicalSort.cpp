9217b1783e1f626440d544fd07ac07a914b1323b
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-02-10 15:09:33 -0300

    Create topologicalSort.cpp

---
+++ codes/graphs/topologicalSort.cpp
@@ -0,0 +1,31 @@
+// Caso exista uma ordem em um grafo direcionado 
+// retorna true
+// Comlexidade: O(V+E) | Spaco: O(V)
+
+bool topoLogicalSort(vector<vector<int>>& g, vector<int>& deg){
+	int n = g.size();
+	
+	queue<int>q;
+	
+	for(int i = 0; i < n; i++)
+		if(deg[i] == 0) q.push(i);
+	
+	int idx = 0;
+
+	order.assign(n, 0);
+
+	while(!q.empty()){
+		int v = q.front(); q.pop();
+		order[idx] = v; idx++;
+
+		for(int& viz : g[v]){
+			deg[viz]--;
+
+			if(deg[viz] == 0) q.push(viz);
+		}
+	}
+
+	if(idx != n) return false;
+
+	return true;
+}
