5f8256a6cbc97b9c0355101f74c83e63c68356e0
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-02-17 16:07:44 -0300

    Create floydWarshall.cpp

---
+++ codes/graphs/floydWarshall.cpp
@@ -0,0 +1,16 @@
+// Complexidade: O(n^3)
+// Lembrar de receber arestas no padrao
+// d[v][u] = d[u][v] = min(d[v][u], (ll)c);
+const int MAX = 500;
+vector<vector<ll>> d(MAX+10, vector<ll>(MAX+10, LINF));
+
+void floyd(int n){
+	for(int i = 0; i < n; i++) d[i][i] = 0;
+
+	for(int k = 0; k < n; k++)
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)
+				// Considera arestas com peso neg
+				if(d[i][k] < LINF and d[k][j] < LINF)
+					d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+}
