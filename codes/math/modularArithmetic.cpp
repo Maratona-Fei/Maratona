b6fac7340ae9ef68a7a87641e47ae018c77afa8d
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-02-27 17:13:49 -0300

    Update modularArithmetic.cpp

--- codes/math/modularArithmetic.cpp
+++ codes/math/modularArithmetic.cpp
@@ -1,15 +1,32 @@
-int mul(int x, int y){
-	return (1LL * x * y) % mod;
+ll mul(ll x, ll y, ll mod){
+	return (x * y) % mod;
 }
 
-// Complexidade: O(logn)
-int exp(int x, ll n){
-	int ans = 1 % mod;
+ll exp(ll x, ll n, ll mod){
+	ll ans = 1 % mod;
 
 	while(n > 0){
-		if(n & 1) ans = mul(x, ans);
-		x = mul(x, x);
+		if(n & 1) ans = mul(x, ans, mod);
+		x = mul(x, x, mod);
 		n >>= 1;
 	}
 	return ans;
+}
+
+void factorial(ll mod){
+	fac[0] = 1;
+	for(int i = 1; i <= MAX; i++){
+			fac[i] = mul(fac[i-1], i, mod);
+	}
+}
+
+void inverses(ll mod){
+	inv[MAX] = exp(fac[MAX], mod - 2, mod);
+	for(int i = MAX; i >= 1; i--){
+		inv[i-1] = mul(inv[i], i, mod);
+	}
+}
+
+ll choose(ll n, ll k, ll mod){
+	return fac[n] * inv[k] % mod * inv[n-k] % mod;
 }
