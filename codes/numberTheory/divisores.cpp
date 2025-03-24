b81ea7094e2a59725518b3eaa0a4d38b5fb3f4c2
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-03-24 17:02:06 -0300

    Update divisores.cpp

--- codes/numberTheory/divisores.cpp
+++ codes/numberTheory/divisores.cpp
@@ -1,20 +1,3 @@
-ll numeroDeDivisores(ll num){
-    ll total = 1;
-    for(int i = 2 ; (ll)i * i <= num ; i++){
-            if(num % i == 0){
-                    int e = 0;
-                    do{
-                       e++;
-                       num /= i;
-                        }while(num % i == 0);
-                        total *= e + 1;
-                }
-        }
-        if(num > 1)
-            total *= 2;
-        return total;
-}
-
 //Complexidade(N^1/3)
 //Nota: precisa de crivo para a execucao
 int numeroDivisores(int num){
@@ -39,27 +22,80 @@ int numeroDivisores(int num){
     return ans;
 }
 
-ll somaDosDivisores(ll num){
-    ll total = 1;        
+// Complexidade: O(sqrt(num))
+ll numberOfDivisors(ll num){
+	ll total = 1;
 
-    for(int i = 2 ; (ll)i * i <= num ; i++){
-            if(num % i == 0){
-                    int e = 0; 
-                    do{
-                        e++;
-                        num /= i;
-                        }while(num % i == 0);
+	for(int i = 2; (ll)i*i <= num; i++){
+		if(num % i == 0){
+			int e = 0;
+			do{
+				e++;
+				num /= i;
+			} while(num % i == 0);
+			total *= e + 1;
+		}
+	}
+	if(num > 1) total *= 2;
 
-                    ll sum = 0 , pow = 1;
-                    do{
-                        sum += pow;
-                        pow *= i;
-                        }while(e-- > 0);
-                        total *= sum;
-                }
-        }
-        if(num > 1)
-            total *= (1 + num);
+	return total;
+}
 
-        return total;
+// Soma de todos os divisores de num
+// Complexidade: O(sqrt(num))
+ll sumOfDivisors(ll num){
+	ll total = 1;
+
+	for(int i = 2; (ll)i*i <= num; i++){
+		if(num % i == 0){
+			int e = 0;
+
+			do{
+				e++;
+				num /= i;
+			} while(num % i == 0);
+
+			ll sum = 0, pow = 1;
+			
+			do{
+				sum += pow;
+				pow *= i;
+			} while(e-- > 0);
+			
+			total *= sum;
+		}
+	}
+
+	if(num > 1) total *= (1 + num);
+
+	return total;
+}
+
+const int two_mod_inv = 500000004;
+
+ll total_sum(ll start, ll end){
+	return ((((end - start + 1) % mod) * ((start + end) % mod) % mod) * two_mod_inv % mod);
+}
+// Soma dos divisores de 1 a n
+// Complexidade: O(confia)
+ll sum_divisors(ll n){
+	ll total = 0;
+
+	ll at = 1;
+
+	while (at <= n) {
+
+		ll add_amt = n / at;  // Our divisor to process
+
+		// The largest number that still has the same value of q
+
+		ll last_same = n / add_amt;
+
+		total = (total + add_amt * total_sum(at, last_same)) % mod;
+
+		at = last_same + 1;
+
+	}
+
+	return total;
 }
