c7dde23f626e1d6d6fffd007cdc911afe806a461
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-03-27 16:30:22 -0300

    Create bitwiseOperations.cpp

---
+++ codes/basics/bitwiseOperations.cpp
@@ -0,0 +1,32 @@
+// verifica se o bit k eh set
+int check_kth_bit(int x, int k){
+	return (x >> k) & 1;
+}
+
+// printa a posicao de todos os sets
+void print_on_bits(int x){
+	for(int k = 0; k < 32; k++){
+		if(check_kth_bit(x, k)){
+			cout<<k<<' ';
+		}
+	}
+}
+
+// seta o bit k
+int set_kth_bit(int x, int k){
+	return x | (1 << k);
+}
+
+// unseta o bit k
+int unset_kth_bit(int x, int k){
+	return x & (~(1 << k));
+}
+
+// toggle o o bit k
+int toggle_kth_bit(int x, int k){
+	return x ^ (1 << k);
+}
+
+bool check_power_of_2(int x){
+	return __builtin_popcount(x) == 1;
+}
