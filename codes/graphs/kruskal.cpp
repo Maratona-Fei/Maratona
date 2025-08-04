3c1ee173f86d23d3cdb4cca66f3cea3bd05f30cc
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-08-04 16:32:00 -0300

    Create kruskal.cpp

---
+++ codes/graphs/kruskal.cpp
@@ -0,0 +1,34 @@
+// Kruskal
+
+// precisa do DSU
+// complexidade: O(mlogn)
+
+struct edge {
+	int u, v, w;
+	bool operator<(edge const& other){
+		return w < other.w;
+	}
+};
+
+
+int kruskal(vector<edge> g){
+	int n = g.size();
+	
+	DSU dsu(n);
+
+	int cost = 0;
+
+	vector<edge> mst(n);
+
+	sort(g.begin(), g.end());
+
+	for(edge e : g){
+		if(dsu.find(e.u) != dsu.find(e.v)){
+			cost += e.w;
+			mst.push_back(e);
+			dsu.unite(e.u, e.v);
+		}
+	}
+
+	return cost;
+}
